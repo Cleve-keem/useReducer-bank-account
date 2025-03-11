2df533b3f01167530b74efb30c25b05617456df2
Hackhim <belloharyourmidey@gmail.com>
2025-03-11 01:41:12 -0700

    fixed the request loan, withdraw, deposit, openAcc

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,56 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import "./App.css";
 
+const initialState = {
+  balance: 0,
+  loan: 0,
+  isValid: false,
+};
+
+const DEBIT_150 = 150;
+const WITHDRAW_50 = 50;
+const LOAN_5000 = 5000;
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "openAcc":
+      return {
+        ...state,
+        balance: 500,
+        isValid: true,
+      };
+
+    case "deposit":
+      return {
+        ...state,
+        balance: state.balance > 0 ? state.balance + DEBIT_150 : state.balance,
+      };
+
+    case "withdraw":
+      return {
+        ...state,
+        balance:
+          state.balance >= WITHDRAW_50
+            ? state.balance - WITHDRAW_50
+            : state.balance,
+      };
+    case "requestLoan":
+      return {
+        ...state,
+        balance: state.balance + LOAN_5000,
+        loan: LOAN_5000,
+      };
+    default: {
+      return state;
+    }
+  }
+}
+
 function App() {
-  const [balance, setBalance] = useState(0);
-  const [loan, setLoan] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { balance, loan, isValid } = state;
+  const isDisable = !isValid;
 
   return (
     <div className="app">
@@ -11,12 +58,29 @@ function App() {
       <p>Balance: {balance}</p>
       <p>Loan: {loan}</p>
       <div className="buttons">
-        <button>Open Account</button>
-        <button>Deposit</button>
-        <button>Withdraw</button>
-        <button>Request a loan of 5000</button>
-        <button>Pay loan</button>
-        <button>Close Account</button>
+        <button onClick={() => dispatch({ type: "openAcc" })}>
+          Open Account
+        </button>
+        <button
+          onClick={() => dispatch({ type: "deposit" })}
+          disabled={isDisable}
+        >
+          Deposit
+        </button>
+        <button
+          onClick={() => dispatch({ type: "withdraw" })}
+          disabled={isDisable}
+        >
+          Withdraw
+        </button>
+        <button
+          onClick={() => dispatch({ type: "requestLoan" })}
+          disabled={isDisable}
+        >
+          Request a loan of 5000
+        </button>
+        <button disabled={isDisable}>Pay loan</button>
+        <button disabled={isDisable}>Close Account</button>
       </div>
     </div>
   );
